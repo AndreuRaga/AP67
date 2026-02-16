adbde5880b9ce6758c43a75ea323a69c3ba1f51e
Andreu Raga Català <anraca01@alumnatflorida.es>
2026-02-16 11:08:09 +0100

    Clases creadas

---
+++ models/EntidadEstelar.php
@@ -0,0 +1,50 @@
+<?php
+class EntidadEstelar {
+    private $id;
+    private $nombre;
+    private $planetaOrigen;
+    private $nivelEstabilidad;
+
+    public function __construct($id, $nombre, $planetaOrigen, $nivelEstabilidad) {
+        $this->id = $id;
+        $this->nombre = $nombre;
+        $this->planetaOrigen = $planetaOrigen;
+        $this->nivelEstabilidad = $nivelEstabilidad;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getPlanetaOrigen() {
+        return $this->planetaOrigen;
+    }
+
+    public function setPlanetaOrigen($planetaOrigen) {
+        $this->planetaOrigen = $planetaOrigen;
+    }
+
+    public function getNivelEstabilidad() {
+        return $this->nivelEstabilidad;
+    }
+
+    public function setNivelEstabilidad($nivelEstabilidad) {
+        $this->nivelEstabilidad = $nivelEstabilidad;
+    }
+
+    public function reaccionar() {
+        return "Se ha encontrado una entidad estelar: " . $this->nombre . " del planeta " . $this->planetaOrigen . " con un nivel de estabilidad de " . $this->nivelEstabilidad . ".";
+    }
+}
