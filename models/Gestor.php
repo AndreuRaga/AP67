6c3a49034aa942e84bdf3e6a4715102e2f26711f
Andreu Raga Català <anraca01@alumnatflorida.es>
2026-02-17 11:23:50 +0100

    Gestor: funciones explorador y modificacion por entidad

--- models/Gestor.php
+++ models/Gestor.php
@@ -10,25 +10,66 @@ class Gestor {
         $_SESSION['entidades'][] = $entidad;
     }
 
-    public function explorador() {
-        return $_SESSION['entidades'];
+    public function exploradorFormasDeVida() {
+        $formasDeVida = [];
+        foreach ($_SESSION['entidades'] as $entidad) {
+            if ($entidad instanceof FormaDeVida) {
+                $formasDeVida[] = $entidad;
+            }
+        }
+        return $formasDeVida;
     }
 
-    public function modificacion($entidad) {
-        foreach ($_SESSION['entidades'] as $i => $entidadSesion) {
-            if ($entidadSesion->getId() == $entidad->getId()) {
-                $_SESSION['entidades'][$i]->setNombre($entidad->getNombre());
-                $_SESSION['entidades'][$i]->setPlanetaOrigen($entidad->getPlanetaOrigen());
-                $_SESSION['entidades'][$i]->setNivelEstabilidad($entidad->getNivelEstabilidad());
-                if ($entidad instanceof FormaDevida) {
+    public function exploradorMineralesRaros() {
+        $mineralesRaros = [];
+        foreach ($_SESSION['entidades'] as $entidad) {
+            if ($entidad instanceof MineralRaro) {
+                $mineralesRaros[] = $entidad;
+            }
+        }
+        return $mineralesRaros;
+    }
 
-                }
-                if ($entidad instanceof MineralRaro) {
+    public function exploradorArtefactosAntiguos() {
+        $artefactosAntiguos = [];
+        foreach ($_SESSION['entidades'] as $entidad) {
+            if ($entidad instanceof ArtefactoAntiguo) {
+                $artefactosAntiguos[] = $entidad;
+            }
+        }
+        return $artefactosAntiguos;
+    }
 
-                }
-                if ($entidad instanceof ArtefactoAntiguo) {
+    public function modificacionFormaDeVida($id, $nombre, $planetaOrigen, $nivelEstabilidad, $dieta) {
+        foreach ($_SESSION['entidades'] as $i => $entidad) {
+            if ($entidad->getId() == $id) {
+                $_SESSION['entidades'][$i]->setNombre($nombre);
+                $_SESSION['entidades'][$i]->setPlanetaOrigen($planetaOrigen);
+                $_SESSION['entidades'][$i]->setNivelEstabilidad($nivelEstabilidad);
+                $_SESSION['entidades'][$i]->setDieta($dieta);
+            }
+        }
+    }
 
-                }
+    public function modificacionMineralRaro($id, $nombre, $planetaOrigen, $nivelEstabilidad, $dureza) {
+        foreach ($_SESSION['entidades'] as $i => $entidad) {
+            if ($entidad->getId() == $id) {
+                $_SESSION['entidades'][$i]->setNombre($nombre);
+                $_SESSION['entidades'][$i]->setPlanetaOrigen($planetaOrigen);
+                $_SESSION['entidades'][$i]->setNivelEstabilidad($nivelEstabilidad);
+                $_SESSION['entidades'][$i]->setDureza($dureza);
+            }
+        }
+    }
+
+    public function modificacionArtefactoAntiguo($id, $nombre, $planetaOrigen, $nivelEstabilidad, $antiguedad) {
+        foreach ($_SESSION['entidades'] as $i => $entidad) {
+            if ($entidad->getId() == $id) {
+                $_SESSION['entidades'][$i]->setNombre($nombre);
+                $_SESSION['entidades'][$i]->setPlanetaOrigen($planetaOrigen);
+                $_SESSION['entidades'][$i]->setNivelEstabilidad($nivelEstabilidad);
+                $_SESSION['entidades'][$i]->setAntiguedad($antiguedad);
+                
             }
         }
     }
