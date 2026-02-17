f6b1bad1c0989e1161235aaeef44737e8941f0a7
Andreu Raga Català <anraca01@alumnatflorida.es>
2026-02-17 13:24:08 +0100

    Arreglada función editarFormaDeVida()

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -44,6 +44,12 @@ class Controller {
 
     public function editarFormaDeVida() {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $id = $_POST['id'];
+            $nombre = $_POST['nombre'];
+            $planetaOrigen = $_POST['planetaOrigen'];
+            $nivelEstabilidad = $_POST['nivelEstabilidad'];
+            $dieta = $_POST['dieta'];
+            $this->gestor->modificacionFormaDeVida($id, $nombre, $planetaOrigen, $nivelEstabilidad, $dieta);
             header("Location: index.php");
             exit();
         }
